b67e339cbf5627fa8c387d195a4650f059f65ba6
wincenttjoi <wincenttsjoi@gmail.com>
2021-06-02 00:16:39 +0800

    Update deploy to bank on network and mint

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -12,7 +12,7 @@ contract Token is ERC20 {
         minter = msg.sender;
     }
 
-    function passMinterRole(address dBank) public returns (Bool) {
+    function passMinterRole(address dBank) public returns (bool) {
         require(msg.sender == minter);
         minter = dBank;
 
